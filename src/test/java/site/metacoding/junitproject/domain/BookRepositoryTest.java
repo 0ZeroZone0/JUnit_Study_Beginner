1bbefa9b5e4390a1b3d1b11ade8fd5ee329338fe
0Zone <00zero0zone00@gmail.com>
2023-10-27 23:04:23 +0900

    select 테스트

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -2,6 +2,9 @@ package site.metacoding.junitproject.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,12 +15,29 @@ public class BookRepositoryTest {
     @Autowired // DI
     private BookRepository bookRepository;
 
+    // @BeforeAll //테스트 시작전에 한번만 실행
+    @BeforeEach // 각 테스트 시작전에 한번씩 실행
+    public void 데이터준비() {
+        System.out.println("===================================");
+
+        String title = "junit";
+        String author = "스프링부트";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+
+    } // 트랜잭션 종료가 됐다면 책목록보기 함수 단위테스트할때 오류가 났어야했는데 안남.
+      // 가정 1 : [데이터준비() + 1 책등록] (하나의 트랜잭션) , [데이터준비() + 2 책목록보기] (하나의 트랜잭션) -> 검증 완료
+      // 가정 2 : [데이터준비() + 1 책등록 + 데이터준비() + 2 책목록보기] (하나의 트랜잭션) -> 검증 실패
+
     // 1. 책 등록
     @Test
     public void 책등록_test() {
-        // given (데이터 준비)
-        String title = "junit5";
-        String author = "메타코딩";
+        // given
+        String title = "junit";
+        String author = "스프링부트";
         Book book = Book.builder()
                 .title(title)
                 .author(author)
@@ -28,13 +48,43 @@ public class BookRepositoryTest {
 
         // then (검증)
         assertEquals(title, bookPS.getTitle());
-         assertEquals(author, bookPS.getAuthor());
-         
-    }
+        assertEquals(author, bookPS.getAuthor());
+
+    } // 트랜잭션 종료 (저장된 데이터를 초기화함)
 
     // 2. 책 목록보기
+    @Test
+    public void 책목록보기_test() {
+        // given
+        String title = "junit";
+        String author = "스프링부트";
+
+        // when
+        List<Book> booksPS = bookRepository.findAll();
+
+        System.out.println("책 사이즈 ========================= : " + booksPS.size());
+
+        // then
+        assertEquals(title, booksPS.get(0).getTitle());
+        assertEquals(author, booksPS.get(0).getAuthor());
+
+    }// 트랜잭션 종료 (저장된 데이터를 초기화함)
 
     // 3. 책 한건보기
+    @Test
+    public void 책한건보기_test() {
+        // given
+        String title = "junit";
+        String author = "스프링부트";
+
+        // when
+        Book bookPS = bookRepository.findById(1L).get();
+
+        // then
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
+
+    }
 
     // 4. 책 수정
 
