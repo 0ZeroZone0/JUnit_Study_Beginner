3d4bb1f8152145f9bc4f2d66869ca8ad78918923
0Zone <00zero0zone00@gmail.com>
2023-11-01 20:40:37 +0900

    서비스레이어 책 한건보기

--- src/main/java/site/metacoding/junitproject/service/BookService.java
+++ src/main/java/site/metacoding/junitproject/service/BookService.java
@@ -1,6 +1,7 @@
 package site.metacoding.junitproject.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -34,8 +35,17 @@ public class BookService {
     }
 
     // 3. 책한건보기
+    public BookRespDto 책한건보기(Long id){
+        Optional<Book> bookOP = bookRepository.findById(id);
+        if(bookOP.isPresent()){
+            return new BookRespDto().toDto(bookOP.get());
+        }else{
+            throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
+        }
 
-    // 4. 책삭제
+
+
+
 
     // 5. 책수정
 
