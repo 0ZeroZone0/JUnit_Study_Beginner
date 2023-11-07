ac443c441d09402530baf0f96b34d99c088a8243
0Zone <00zero0zone00@gmail.com>
2023-11-07 21:10:47 +0900

    컨트롤러 레이어 본코드 책 저장하기 유효성 검사

--- src/main/java/site/metacoding/junitproject/web/BookApiController.java
+++ src/main/java/site/metacoding/junitproject/web/BookApiController.java
@@ -1,11 +1,17 @@
 package site.metacoding.junitproject.web;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import site.metacoding.junitproject.service.BookService;
 import site.metacoding.junitproject.web.dto.request.BookSaveReqDto;
@@ -22,10 +28,27 @@ public class BookApiController {
     // key=value&key=value
     // {"key" : value , "key" : value}
     @PostMapping("/api/v1/book")
-    public ResponseEntity<?> saveBook(@RequestBody BookSaveReqDto bookSaveReqDto) {
+    public ResponseEntity<?> saveBook(@RequestBody @Valid BookSaveReqDto bookSaveReqDto, BindingResult bindingResult) {
         BookRespDto bookRespDto = bookService.책등록하기(bookSaveReqDto);
-        CMRespoDto<?> cmRespoDto = CMRespoDto.builder().code(1).msg("글 저장 성공").body(bookRespDto).build();
-        return new ResponseEntity<>(cmRespoDto, HttpStatus.CREATED); // 201 = insert
+
+        // 후에 AOP 처리하는게 좋음
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError fe : bindingResult.getFieldErrors()) {
+                errorMap.put(fe.getField(), fe.getDefaultMessage());
+            }
+            System.out.println("=============================");
+            System.out.println(errorMap.toString());
+            System.out.println("=============================");
+
+            return new ResponseEntity<>(
+                    CMRespoDto.builder().code(-1).msg(errorMap.toString()).body(bookRespDto).build(),
+                    HttpStatus.CREATED); // 400= 요청이 잘못옴
+        }
+
+        return new ResponseEntity<>(CMRespoDto.builder().code(1).msg("글 저장 성공").body(bookRespDto).build(),
+                HttpStatus.CREATED); // 201 = insert
+
     }
 
     // 2. 책 목록보기
